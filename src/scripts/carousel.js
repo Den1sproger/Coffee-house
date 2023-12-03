c7dc12d27070281975bf51039c32707133adb4e9
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-12-03 13:58:13 +0300

    refactor: add comments to carousel script

--- src/scripts/carousel.js
+++ src/scripts/carousel.js
@@ -46,6 +46,7 @@ async function createCoffeeCarusel() {
 }
 
 
+// set first coffee slide after styles loading
 function setBeginSlide() {
   const progressBars = document.querySelectorAll('.progress-bar')
   progressBars[currentSlideIndex].classList.toggle('active')
@@ -57,17 +58,20 @@ function setCoffeeSlide(slides, controls, oldSlideIndex) {
   clearTimeout(timerId)
   startSlider()
 
+  // offset list of coffee slides
   startX = null
   currentX = null
   const listOfSlides = document.querySelector('.slide ul')
   const translate = (100 / slides.length) * currentSlideIndex
   listOfSlides.style.transform = `translateX(-${translate}%)`
 
+  // disable old progress bar
   const oldBar = controls[oldSlideIndex]
   oldBar.classList.toggle('active')
   oldBar.style.transition = 'width 0s'
   oldBar.style.width = '0'
 
+  // enable current progress bar
   const currentBar = controls[currentSlideIndex]
   currentBar.classList.toggle('active')
   currentBar.style.transition = `width ${sliderInterval / 1000 + 1}s`
@@ -136,6 +140,7 @@ function handlerTouchEnd() {
 
 
 
+// start the countdown before automatic switch to the next slide
 function startSlider(interval=sliderInterval) {
   timePass = 0
   timeLeft = 0
@@ -150,6 +155,7 @@ function stopSlider() {
   timePass = timePass + Date.now() - lastTime
   timeLeft = sliderInterval - timePass
 
+  // stop the progress bar
   currentProgressBar = document.querySelector('.progress-bar.active')
   currentProgressBar.classList.toggle('stop')
   currentProgressBar.style.width = getComputedStyle(currentProgressBar).width
@@ -158,9 +164,11 @@ function stopSlider() {
 
 
 function continueSlider() {
+  // continue the progress bar
   currentProgressBar.classList.toggle('stop')
   currentProgressBar.style.width = '100%'
   currentProgressBar.style.transition = `width ${timeLeft / 1000 + 1}s`
+  // start slider with the left time
   startSlider(timeLeft)
 }
 
